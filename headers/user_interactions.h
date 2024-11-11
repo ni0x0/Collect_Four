52e461f82e26412601e3b284f3bdf9023896aaba
ni0x0 <nik.x.i0ta@gmail.com>
2024-11-12 01:42:01 +0300

    добавлен makefile

--- headers/user_interactions.h
+++ headers/user_interactions.h
@@ -1,8 +1,6 @@
 #include "outp_functions.h"
 #include "logic.h"
 
-uint8_t choose_game_mode();
-
-uint8_t get_field_width();
-
-void game_process(struct GAME_DATA * game_data);
+uint8_t choose_game_mode();                       // Выбор режима: 1 или 2 игрока
+uint8_t get_field_width();                        // Задание пользователем желаемой ширины поля
+void game_process(struct GAME_DATA * game_data);  // Игровой процесс заполнения поля фишками
